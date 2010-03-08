d52bbd2f194b0ff90f915509ecb9dc5e40de171d
Gilles Mathurin <gillesmathurin@iMac-de-Beatrice-Campier.local>
2010-03-08 12:51:32 -0400

    Written a spec for the Car model

--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -3,11 +3,26 @@ require 'spec_helper'
 describe Car do
   before(:each) do
     @valid_attributes = {
-      
+      :annee => 2009,
+      :immatriculation => "value for immatriculation",
+      :kilometrage => 20000,
+      :modele => "value for modele",
+      :energy => "value for energy",
+      :boite_vitesse => "value for boite_vitesse",
+      :price => 3500
     }
   end
 
   it "should create a new instance given valid attributes" do
+    Car.make
     Car.create!(@valid_attributes)
   end
+  
+  it "requires an integer value for annee" do
+    @car = Car.new(@valid_attributes.except(:annee))
+    @car.should_not be_valid
+    @car = Car.new(@valid_attributes[:annee => "azerty"])
+    @car.should have(1).error_on(:annee)
+  end
+  
 end
