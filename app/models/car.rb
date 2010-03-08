c6649388609890b476b0876fa021aadb47ec22cb
Gilles Mathurin <gillesmathurin@iMac-de-Beatrice-Campier.local>
2010-03-08 12:36:12 -0400

    Configured the machinist/blueprints.rb file for the Vehicle and Car models

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,4 @@
 class Car < Vehicle
+  validates_presence_of :annee, :modele, :price, :kilometrage, :energy, :boite_vitesse,
+   :on => :create, :message => "can't be blank"
 end
--- spec/blueprints.rb
+++ spec/blueprints.rb
@@ -2,11 +2,22 @@ require 'machinist/active_record'
 require 'sham'
 require 'faker'
 
+def String.random_alphanumeric(size=16)
+  s = ""
+  size.times { s << (i = Kernel.rand(62); i += ((i < 10) ? 48 : ((i < 36) ? 55 : 61 ))).chr }
+  s
+end
+
 Sham.define do
   modele  { Faker::Lorem.words }
   description  { Faker::Lorem.paragraph }
   price { rand(50000) }
-  annee { (1960..Date.today.year).map.rand }
+  annee { (1960..Date.today.year).to_a.rand }
+  kilometrage { (0..200000).to_a.rand }
+  serialnumber { String.random_alphanumeric }
+  immatriculation { String.random_alphanumeric(8) }
+  energy { Vehicle::ENERGY.rand }
+  boite_vitesse { Vehicle::VITESSE.rand }
 end
 
 Vehicle.blueprint do
@@ -21,4 +32,8 @@ Car.blueprint do
   description
   price
   annee
+  kilometrage
+  immatriculation
+  energy
+  boite_vitesse
 end
