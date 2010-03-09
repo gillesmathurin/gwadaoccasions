4daa7d7f79c91cb7459de401d84681bce29ad129
Gilles Mathurin <gillesmathurin@iMac-de-Beatrice-Campier.local>
2010-03-08 22:19:46 -0400

    Feature : Visitor Search, refactored a scenario and added another one

--- features/step_definitions/search_steps.rb
+++ features/step_definitions/search_steps.rb
@@ -2,16 +2,21 @@ require 'machinist/active_record'
 require 'sham'
 require 'faker'
 
-Given /^A Voiture Category$/ do
-  Category.create!(:name => "Voiture")
+
+Given /^There is a "([^\"]*)" Category$/ do |category|
+  Category.create!(:name => category)
 end
 
-Given /^A car with a 6000 € price$/ do
-  @car = Voiture.make(:price => 6000, :modele => "Peugeot 206")
+Given /^There is car priced "([^\"]*)" €$/ do |arg1|
+  @car = Voiture.make(:price => arg1, :modele => "Peugeot 206")
 end
 
 Given /^There is no vehicles of the week selection$/ do
   @vehicles_of_the_week = []
+end
+
+Given /^I am on the root page$/ do
+  visit root_path
 end
 
 When /^I search for cars with price between 0 and 10 000 €$/ do
@@ -22,14 +27,25 @@ When /^I search for cars with price between 0 and 10 000 €$/ do
   click_button "Chercher"  
 end
 
-Then /^I should open the page$/ do
-end
-
 Then /^I should see the car$/ do
-  # response.should have_selector(".search_results") do |selector|
-  #   selector.should have_selector("div.result") do |div|
-  #     div.should contain("#{@car.modele} - Annee : #{@car.display_year}")
-  #   end
-  # end
   response.should contain("#{@car.modele} - Année : #{@car.display_year} - #{@car.kilometrage} km - #{@car.price} €")
 end
+
+# 2nd scenario :
+Given /^There is car priced "([^\"]*)" € with "([^\"]*)" km made$/ do |arg1, arg2|
+  @car = Voiture.make(:price => arg1, :kilometrage => arg2, :modele => "Toyota Yaris")
+end
+
+
+When /^I search for "([^\"]*)" priced between "([^\"]*)" and "([^\"]*)" € with at most "([^\"]*)"$/ do |arg1, arg2, arg3, arg4|
+  visit root_path
+  select arg1, :from => "search_category"
+  select arg2, :from => "search_minprice"
+  select arg3, :from => "search_maxprice"
+  select arg4, :from => "search_maxkilometrage"
+  click_button "Chercher"
+end
+
+
+
+
--- features/visitor_searches.feature
+++ features/visitor_searches.feature
@@ -4,11 +4,18 @@ Feature: Visitor Search
 	As a visitor
 	I want to use a search form to find vehicles
 	
-	Scenario: Looking for cars with price criteria
-	Given A Voiture Category
-	And A car with a 6000 € price
+	Scenario: Find Voitures (cars) based on with price criteria
+	Given I am on "the homepage"
+	And There is a "Voiture" Category
+	And There is car priced "6000" €
 	And There is no vehicles of the week selection
 	When I search for cars with price between 0 and 10 000 €
-	Then I should open the page
-	And I should see the car
+	Then I should see the car
+	
+	Scenario: Find Voitures (cars) based on Type, Price and Kilometrage
+	Given I am on "the homepage"
+	And There is a "Voiture" Category
+	And There is car priced "5000" € with "60000" km made
+	When I search for "Voiture" priced between "2000" and "6000" € with at most "70 000 Km"
+	Then I should see the car
 	
