03eb4459af7114344b68e5a050c199b11c6d2d6a
Gilles MATHURIN <gillesmath@macmini.local>
2010-03-18 10:22:00 -0400

    Fixed migration issue

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@ RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 Rails::Initializer.run do |config|
-  config.gem 'paperclip'
+  config.gem 'paperclip', :version => '2.3.1.1'
   config.gem 'will_paginate', :lib => 'will_paginate'
   config.gem 'webrat', :lib => false
   config.gem 'cucumber', :lib => false
