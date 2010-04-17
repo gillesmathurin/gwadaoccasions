c0263f910fb43de8ff2740e4196925bac360df40
Gilles Mathurin <gillesmathurin@iMac-de-Beatrice-Campier.local>
2010-04-17 13:42:47 -0400

    Wheneverized the app

--- app/models/background_task.rb
+++ app/models/background_task.rb
@@ -4,9 +4,13 @@ class BackgroundTask
   ProviderLeadsStruct = Struct.new(:provider, :vehicles_leads)
   
   def self.sends_leads_list_to_providers
+    
+    # Arrays to store registered users matching vehicles
     @vehicle_leads_array = []
+    # Array to store provider and leads user association
     @provider_leads_array = []
     
+    # Build the Matching datas sets
     Provider.all.each do |provider|
       provider.vehicles.available.each do |vehicle|
         lead_users = []
@@ -18,14 +22,15 @@ class BackgroundTask
       @provider_leads_array << ProviderLeadsStruct.new(provider, @vehicle_leads_array)
     end
     
+    # Send the matching datas sets to each provider
     @provider_leads_array.each do |match_list|
       begin
         BackgroundTaskMailer.deliver_leads_list(match_list)
       rescue Exception => e
         Rails.logger.info("#{e}")
-      end
-      
+      end      
     end
+    
   end
   
 end
